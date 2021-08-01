81bb26160000f85b847dc1d90ddd97c4a70865e3
cine6m <cine99m@gmail.com>
2021-08-02 04:32:43 +0900

    idle 애니메이션추가

--- idle.cpp
+++ idle.cpp
@@ -5,6 +5,14 @@
 HRESULT idle::init()
 {
 	_playerImg = IMAGEMANAGER->findImage("PLAYER_idle");
+
+	_playerAniL = new animation;
+	_playerAniL->init(_playerImg->getWidth(), _playerImg->getHeight(), 12, 2);
+	_playerAniL->setPlayFrame(5, 0, false, false);
+	_playerAniL->setFPS(1);
+
+
+
 	return S_OK;
 }
 
