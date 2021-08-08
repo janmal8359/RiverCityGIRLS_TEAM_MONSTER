c9c32dafb92f2825ac1945178f852886c9c0e8d9
HANGIL KIM <87747812+AgentBear117@users.noreply.github.com>
2021-08-08 15:29:50 +0900

    2021.08.08.4

--- firstStage.cpp
+++ firstStage.cpp
@@ -80,7 +80,7 @@ void firstStage::pixelCollision()
 
 
 	////벽 충돌
-	for (int i = _probeLX - 5; i < _probeLX; i++)
+	for (int i = _probeLX - 5; i < _probeRX + 5; i++)
 	{
 		COLORREF color = GetPixel(_first->getMemDC(), i, _player->getShadowY());
 
@@ -120,47 +120,47 @@ void firstStage::pixelCollision()
 		}
 	}
 
-	for (int i = _probeRX; i < _probeLX + 5; i++)
-	{
-		COLORREF color = GetPixel(_first->getMemDC(), i, _player->getShadowY());
+	//for (int i = _probeRX; i < _probeLX + 5; i++)
+	//{
+	//	COLORREF color = GetPixel(_first->getMemDC(), i, _player->getShadowY());
+	//
+	//	int R = GetRValue(color);
+	//	int G = GetGValue(color);
+	//	int B = GetBValue(color);
+	//
+	//	if (!(R == 255 && G == 0 && B == 255))
+	//	{
+	//		//벽
+	//		if (R == 255 && G == 0 && B == 0)//레드
+	//		{
+	//			_player->setSpeed(0);
+	//		}
+	//		//책상 바닥
+	//		else if ((R == 0 && G == 255 && B == 0))//그린
+	//		{
+	//			//_player->setShadowX(-8);
+	//			//_player->setSpeed(6);
+	//			_player->setSpeed(0);
+	//		}
+	//		//아이콘 출현 구간
+	//		else if (R == 0 && G == 255 && B == 255)//민트
+	//		{
+	//			_player->setSpeed(1);
+	//
+	//		}
+	//		//그 이외
+	//		else
+	//		{
+	//			_player->setSpeed(6);
+	//		}
+	//	}
+	//	else
+	//	{
+	//		_player->setSpeed(6);
+	//	}
+	//}
 
-		int R = GetRValue(color);
-		int G = GetGValue(color);
-		int B = GetBValue(color);
-
-		if (!(R == 255 && G == 0 && B == 255))
-		{
-			//벽
-			if (R == 255 && G == 0 && B == 0)//레드
-			{
-				_player->setSpeed(0);
-			}
-			//책상 바닥
-			else if ((R == 0 && G == 255 && B == 0))//그린
-			{
-				//_player->setShadowX(-8);
-				//_player->setSpeed(6);
-				_player->setSpeed(0);
-			}
-			//아이콘 출현 구간
-			else if (R == 0 && G == 255 && B == 255)//민트
-			{
-				_player->setSpeed(1);
-
-			}
-			//그 이외
-			else
-			{
-				_player->setSpeed(6);
-			}
-		}
-		else
-		{
-			_player->setSpeed(6);
-		}
-	}
-
-	for (int i = _probeTY - 5; i < _probeTY; i++)
+	for (int i = _probeTY - 5; i < _probeBY + 5; i++)
 	{
 		COLORREF color = GetPixel(_first->getMemDC(), _player->getShadowX(), i);
 
@@ -201,45 +201,45 @@ void firstStage::pixelCollision()
 	}
 
 
-	for (int i = _probeBY; i < _probeBY + 2; i++)
-	{
-		COLORREF color = GetPixel(_first->getMemDC(), _player->getShadowX(), i);
-
-		int R = GetRValue(color);
-		int G = GetGValue(color);
-		int B = GetBValue(color);
-
-		if (!(R == 255 && G == 0 && B == 255))
-		{
-			//벽
-			if (R == 255 && G == 0 && B == 0)//레드
-			{
-				_player->setSpeed(0);
-			}
-			//책상 바닥
-			else if ((R == 0 && G == 255 && B == 0))//그린
-			{
-				//_player->setShadowY(+8);
-				//_player->setSpeed(6);
-				_player->setSpeed(0);
-			}
-
-			//아이콘 출현 구간
-			else if (R == 0 && G == 255 && B == 255)//민트
-			{
-				_player->setSpeed(1);
-
-			}
-			//그 이외
-			else
-			{
-				_player->setSpeed(6);
-			}
-		}
-		else
-		{
-			_player->setSpeed(6);
-		}
-	}
+	//for (int i = _probeBY; i < _probeBY + 2; i++)
+	//{
+	//	COLORREF color = GetPixel(_first->getMemDC(), _player->getShadowX(), i);
+	//
+	//	int R = GetRValue(color);
+	//	int G = GetGValue(color);
+	//	int B = GetBValue(color);
+	//
+	//	if (!(R == 255 && G == 0 && B == 255))
+	//	{
+	//		//벽
+	//		if (R == 255 && G == 0 && B == 0)//레드
+	//		{
+	//			_player->setSpeed(0);
+	//		}
+	//		//책상 바닥
+	//		else if ((R == 0 && G == 255 && B == 0))//그린
+	//		{
+	//			//_player->setShadowY(+8);
+	//			//_player->setSpeed(6);
+	//			_player->setSpeed(0);
+	//		}
+	//
+	//		//아이콘 출현 구간
+	//		else if (R == 0 && G == 255 && B == 255)//민트
+	//		{
+	//			_player->setSpeed(1);
+	//
+	//		}
+	//		//그 이외
+	//		else
+	//		{
+	//			_player->setSpeed(6);
+	//		}
+	//	}
+	//	else
+	//	{
+	//		_player->setSpeed(6);
+	//	}
+	//}
 	
 }
