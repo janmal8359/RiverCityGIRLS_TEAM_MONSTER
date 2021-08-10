c397dbba5a6b0fe0dd8d0962c4dfba5e2b874880
LEE HAN BYUL <84006642+cine99m@users.noreply.github.com>
2021-08-10 19:50:20 +0900

    z order 진행중

---
+++ zOrder.cpp
@@ -0,0 +1,27 @@
+#include "pch.h"
+#include "zOrder.h"
+
+zOrder::zOrder()
+{
+}
+
+zOrder::~zOrder()
+{
+}
+
+HRESULT zOrder::init()
+{
+	return S_OK;
+}
+
+void zOrder::release()
+{
+}
+
+void zOrder::update()
+{
+}
+
+void zOrder::render()
+{
+}
---
+++ zOrder.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <vector>
+#include <map>
+#include "gameNode.h"
+
+
+class zOrder : public gameNode
+{
+
+private:
+	typedef<>
+
+
+
+
+public:
+	zOrder();
+	~zOrder();
+
+	HRESULT init();
+	void release();
+	void update();
+	void render();
+
+};
+
