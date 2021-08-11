b6b1a8527ef921f67d2c0aad1c6fb7ed7a96b61c
HANGIL KIM <87747812+AgentBear117@users.noreply.github.com>
2021-08-11 18:50:35 +0900

    오브젝트 매니저 생성
    백터다!!!!!

---
+++ object.cpp
@@ -0,0 +1,27 @@
+#include "pch.h"
+#include "object.h"
+
+object::object()
+{
+}
+
+object::~object()
+{
+}
+
+HRESULT object::init()
+{
+	return S_OK;
+}
+
+void object::release()
+{
+}
+
+void object::update()
+{
+}
+
+void object::render()
+{
+}
